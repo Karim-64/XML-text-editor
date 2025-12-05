c5ea635e8d6712e232f7c299dc8ad0bf7af30b70
Karim Khaled <karimkhaled2k4@gmail.com>
2025-12-05 15:09:02 +0200

    Project init

---
+++ src/xml_editor_core.py
@@ -0,0 +1,45 @@
+"""XMLEditor handles following functionalities for a given XML File
+    1. Checks consistency and correctness of XML file
+    2. Corrects errors in XML File
+    3. Formats/Prettifies XML by adjusting indentations
+    4. Converts XML file to JSON file
+    5. Reduces physical size of XML file by deleting whitespaces and indentations
+    6. Compresses XML File
+    7. Decompresses XML File
+    
+    
+"""
+from xml_tree import XTree
+
+class XMLEditor:
+    def __init__(self,filePath : str, xmlPastedFile = None) -> None:
+        self.tree = XTree(filePath)
+    
+    def verify(self):
+        """Checks consistency and correctness of XML file"""
+        print("Checking Verified")
+        
+    def correct(self):
+        """Corrects errors in XML File"""
+    
+    def format(self):
+        """Formats/Prettifies XML by adjusting indentations"""
+        
+    def convert(self):
+        """Converts XML file to JSON file"""
+        
+    def minify(self):
+        """Reduces physical size of XML file by deleting whitespaces and indentations"""
+        
+    def compress(self):
+        """Compresses XML File"""
+        
+    def decompress(self):
+        """Decompresses XML File"""
+
+    
+        
+    
+    
+        
+    
---
+++ src/xml_tree.py
@@ -0,0 +1,46 @@
+"""XML is a hierarchical data format, and the most natural way
+to represent it is with a tree. We made two classes for this purpose
+1. XTree represents the whole XML document as a tree
+2. XNode represents a single node in this tree. 
+
+Interactions with the whole document (reading and writing to/from files) are
+done on the XTree level.
+Interactions with a single XML element and its sub-element are
+done on the XNode level.
+"""
+from __future__ import annotations
+
+class XNode:
+    """
+    Class that represents a single node in the XTree
+    """
+
+class XTree:
+    """
+    Class that handles all operations on XML files as a custom Tree structure
+    """
+    def __init__(self, filePath = None) -> None:
+        pass
+    
+    def __parse(self, filePath : str) -> XTree | None:
+        """Parses XML file into an XTree
+        
+        Args:
+            filePath(str) : relative file path to .xml file
+        Returns:
+            XTree : A custom Tree structure that represents XML File using XNodes.
+            
+        """
+        return
+    
+    def write(self, filePath : str, tree : XTree) -> None:
+        """Creates new XML file from an XTree
+        
+        Args:
+            filePath(str): relative file path to write .xml file
+            tree(XTree): An XTree object that will produce the .xml file
+        Returns:
+            None
+        """
+        return
+    
