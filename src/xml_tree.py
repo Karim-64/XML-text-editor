cffc7e8c9868adfbbff8a0cd04a145446c8cfbca
Mariyam-1 <mariyammousa1@gmail.com>
2025-12-11 15:02:39 +0200

    Node and Tree

--- src/xml_tree.py
+++ src/xml_tree.py
@@ -11,16 +11,24 @@ done on the XNode level.
 from __future__ import annotations
 
 class XNode:
-    """
-    Class that represents a single node in the XTree
-    """
+    def __init__(self, tag, text=""):
+        self.tag = tag       
+        self.text = text.strip() 
+        self.children = []    
+        self.parent = None 
+
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
 
 class XTree:
     """
     Class that handles all operations on XML files as a custom Tree structure
     """
-    def __init__(self, filePath = None) -> None:
-        pass
+    def __init__(self, root: XNode, file_path: str):
+        # self.parse_xml(file_path)
+        self.root = XNode
+    
     
     def __parse(self, filePath : str) -> XTree | None:
         """Parses XML file into an XTree
@@ -31,7 +39,39 @@ class XTree:
             XTree : A custom Tree structure that represents XML File using XNodes.
             
         """
-        return
+        file = open(f"{file_path}", 'r')
+        xml_string = file.read()
+
+        xml_string = xml_string.strip()
+        i = 0
+        n = len(xml_string)
+        stack = []
+        root = None
+
+        while i < n:
+            if xml_string[i] == "<":
+                j = xml_string.find(">", i)
+                tag_content = xml_string[i + 1:j].strip()
+
+                if tag_content.startswith("/"):
+                    stack.pop()
+                else:
+                    node = XNode(tag_content)
+                    if stack:
+                        stack[-1].add_child(node)
+                    else:
+                        root = node
+                    stack.append(node)
+
+                i = j + 1
+            else:
+                j = xml_string.find("<", i)
+                text_content = xml_string[i:j].strip()
+                if text_content and stack:
+                    stack[-1].text += text_content
+                i = j
+        return root
+        
     
     def write(self, filePath : str, tree : XTree) -> None:
         """Creates new XML file from an XTree
