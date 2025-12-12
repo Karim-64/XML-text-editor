efb24459554af64a65f6a2e6d536573d20e262b9
Karim Khaled <karimkhaled2k4@gmail.com>
2025-12-12 13:05:36 +0200

    switching work to jsonConverter instead of tree-branch

--- src/xml_editor_core.py
+++ src/xml_editor_core.py
@@ -1,19 +1,25 @@
 """XMLEditor handles following functionalities for a given XML File
     1. Checks consistency and correctness of XML file
     2. Corrects errors in XML File
+    
     3. Formats/Prettifies XML by adjusting indentations
-    4. Converts XML file to JSON file
-    5. Reduces physical size of XML file by deleting whitespaces and indentations
+    4. Reduces physical size of XML file by deleting whitespaces and indentations
+
+    
+    5. Converts XML file to JSON file
+
     6. Compresses XML File
     7. Decompresses XML File
     
     
 """
-from xml_tree import XTree
+import json
+from xml_tree import XNode, XTree
 
 class XMLEditor:
     def __init__(self,filePath : str, xmlPastedFile = None) -> None:
         self.tree = XTree(filePath)
+        self.jsonDictionary = {}
     
     def verify(self):
         """Checks consistency and correctness of XML file"""
@@ -25,8 +31,54 @@ class XMLEditor:
     def format(self):
         """Formats/Prettifies XML by adjusting indentations"""
         
-    def convert(self):
+    def convert(self, root : XNode):
         """Converts XML file to JSON file"""
+
+        if(not root):
+            return
+        if(not root.children):
+            return {root.tag : root.text}
+        
+        jsonDictionary = {}
+        for child in root.children:
+            if child.tag in jsonDictionary:
+                if type(jsonDictionary[child.tag]) != list:
+                    jsonDictionary[child.tag] = [jsonDictionary[child.tag]]
+                jsonDictionary[child.tag].append(self.convert(child))
+            else:
+                jsonDictionary.update(self.convert(child))
+        # print(root.tag)
+            
+        return {root.tag : jsonDictionary}
+    
+    def formatJsonDict(self, jsonDictionary : dict):
+        """Prettifies JSON File
+        Args:
+            jsonDictionary : dict : The dictionary returned from convert() function
+        """
+        jsonStr = str(jsonDictionary)
+        res = ""
+        indentation = ""
+        # print(len(jsonStr))
+        for i,char in enumerate(jsonStr):
+            if char == '{' or char == '[':
+                indentation += "    "
+                res += char + '\n' + indentation
+                continue
+            if char == ',' and (jsonStr[i-1] == "'" or jsonStr[i-1] == "}"):
+                res += char + '\n' + indentation
+                continue
+            if char == '}' or char == ']':
+                if(len(indentation) >= 4):
+                    indentation = indentation[:-4]
+                else:
+                    indentation = ""
+                res += '\n' + indentation + char
+                continue 
+            res += char
+        # print(count)
+        return res
+        
         
     def minify(self):
         """Reduces physical size of XML file by deleting whitespaces and indentations"""
@@ -36,7 +88,19 @@ class XMLEditor:
         
     def decompress(self):
         """Decompresses XML File"""
-
+        
+def main():
+    editor = XMLEditor("src/sample.xml")
+    tree = editor.tree
+    root = tree.root
+    jsonDict = editor.convert(root)
+    prettyJsonDict = json.dumps(jsonDict, indent=4)
+    # print(jsonDict)
+    print(prettyJsonDict)
+    # print(editor.formatJsonDict(jsonDict))
+    
+if __name__ == "__main__":
+    main()
     
         
     
